0d4f66dd140032a6d732d2152e1141dc01f358af
kishkisupermarket <kishkisupermarket@hotmail.com>
2025-08-21 19:15:18 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js - Service Worker
+const CACHE_NAME = 'kishki-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/main.js',
+    '/products.json',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
